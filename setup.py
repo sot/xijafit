b35738c9e275a3d416d71fc0d7144457d02e74fb
Matthew Dahmer <matthew.dahmer@gmail.com>
2017-04-26 11:09:02 -0400

    Add tests

--- setup.py
+++ setup.py
@@ -3,15 +3,22 @@ from setuptools import setup
 
 from xijafit import __version__
 
+try:
+    from testr.setup_helper import cmdclass
+except ImportError:
+    cmdclass = {}
+
 setup(
     name='xijafit',
     version=__version__,
     author='Matthew Dahmer',
     author_email='matthew.dahmer@gmail.com',
-    packages=['xijafit'],
+    packages=['xijafit', 'xijafit.tests'],
     url='http://cxc.cfa.harvard.edu/mta/ASPECT/tool_doc/xijafit/',
     license='BSD',
     description='Tool for automating the Xija model fitting process',
     long_description=open('README').read(),
     package_data={'xijafit': ['example_data/*.json']},
+    tests_require=['pytest'],
+
 )
